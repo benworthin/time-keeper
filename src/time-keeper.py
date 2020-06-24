1dcb663df507bc4483d03336ab8ecf480599f1cc
Benjamin Worthington <benworthin@icloud.com>
2020-06-24 15:25:36 -0500

    Created core functionality
    Created it so the user can record a 'clock-in' or a 'clock-out' with the ability to make sure the input is valid. The punch gets recorded in a .txt file with either a 'CLOCK-IN' or 'CLOCK-OUT' and the exact date/time along with a digital signature (currently hardcoded for me) of the user.

--- src/time-keeper.py
+++ src/time-keeper.py
@@ -1,10 +1,23 @@
 import os
 import csv
-# from datetime import date
-# from datetime import time
 from datetime import datetime
+
+digitalSignature = "Benjamin A. Worthington (010895994)"
 
 today = datetime.today()
 now = today.strftime('%m-%d-%y %I:%M %p')
 
-print(now)
+invalidInput = True
+
+while invalidInput:
+    action = input("Type either 'clock-in' or 'clock-out': ")
+    action = action.upper()
+
+    if action == "CLOCK-IN" or action == "CLOCK-OUT":
+        print("Punch Recorded.\n")
+        invalidInput = False
+    else:
+        print("Invalid input, please try again.")
+
+with open('../punch-card.txt', 'a') as punch_card_txt_file:
+    punch_card_txt_file.write(f"{action} at {now} by {digitalSignature}\n")
